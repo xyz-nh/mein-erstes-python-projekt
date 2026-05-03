566f83a822410f0e64f53a1ecfa82d853019b3b7
xyz-nh <nils@harberts.de>
2026-05-03 16:22:10 +0200

    Berechnung mit historischen Kursen hinzugefügt, noch fehlerhaft

--- hallo.py
+++ hallo.py
@@ -1,10 +1,15 @@
-import datetime
+from datetime import date, datetime, timedelta
 import yfinance as yf
 
-symbol = input("Welches Wertpapier-Kürzel möchtest du prüfen? ").upper()
-geburtsjahr = int(input("Wann wurdest du geboren? "))
+symbol = input("Welches Wertpapier? ").upper()
+geburtstag = input("Wann wurdest du geboren? [YYYY-MM-DD]")
+geburtsjahr = int(geburtstag[:4])
+alter = int(date.today().year-geburtsjahr)
 
-alter = int(datetime.date.today().year-geburtsjahr)
+heute = date.today()
+heute_str = heute.strftime("%Y-%m-%d")
+start_datum = geburtstag
+ende_datum = heute_str
 
 try:
     # Daten vom Yahoo Finance Server abrufen
@@ -21,6 +26,45 @@ try:
     print("-" * 30)
 
 except Exception as e:
-    print(f"Fehler: Das Kürzel '{symbol}' konnte nicht gefunden werden oder es gibt ein Netzwerkproblem.")
+    print(f"Fehler Schritt 1: Das Kürzel '{symbol}' konnte nicht gefunden werden oder es gibt ein Netzwerkproblem.")
+
+try:
+    # Daten vom Yahoo Finance Server abrufen
+    ticker = yf.Ticker(symbol)
+    
+    # Daten für diesen Zeitraum abrufen
+    # Wir laden einen kleinen Puffer (z.B. 3 Tage), falls das Zieldatum ein Wochenende war
+    start = start_datum
+    ende = (datetime.strptime(start_datum, "%Y-%m-%d") + timedelta(days=3)).strftime("%Y-%m-%d")
+
+    # Daten abrufen
+    try:
+        data = yf.download(ticker, start=start, end=ende)
+    except Exception as e:
+        print(f"Fehler Schritt 2: Der Abruf der historischen Daten ging schief.")
+
+    # Den Preis extrahieren
+    if not data.empty:
+        # Wir nehmen den ersten verfügbaren Handelstag im Ergebnis
+        erster_verfuegbarer_tag = data.index[0]
+        preis_geburt = data.loc[erster_verfuegbarer_tag, "Close"]
+        
+        print(f"Kurs von {ticker} am Geburtstag: {float(preis):.2f} {waehrung}")
+    else:
+        print("Keine Daten gefunden. War die Börse vielleicht geschlossen?")
+
+    preis_heute = preis
+
+    investment = 10/preis_geburt*preis_heute
+
+    # Ergebnis anzeigen
+    print("-" * 30)
+    print(f"Wertpapier: {symbol}")
+    print(f"Aktueller Kurs: {preis_heute:.2f} {waehrung}")
+    print(f"Heutiger Wert von 10 {waehrung} Investment bei Geburt: {investment:.2f} {waehrung}")
+    print("-" * 30)
+
+except Exception as e:
+    print(f"Fehler Schritt 3: Die Berechnung ging schief.")
 
 print(f"Du müsstest jetzt ungefähr {alter} Jahre alt sein.")
